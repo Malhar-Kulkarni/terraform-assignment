2ab1536afbc4f942ac1754e9f7e3b65649018d17
Malhar-Kulkarni <46686212+Malhar-Kulkarni@users.noreply.github.com>
2021-11-01 23:57:12 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -7,3 +7,15 @@ provider "aws" {
 data "aws_route53_zone" "zone" {
   name = "${var.route53_hosted_zone_name}"
 }
+
+data "template_file" "provision" {
+  template = "${file("${path.module}/provision.sh")}"
+
+  vars {
+    database_endpoint = "${aws_db_instance.default.endpoint}"
+    database_name     = "${var.database_name}"
+    database_user     = "${var.database_user}"
+    region            = "${var.region}"
+    s3_bucket_name    = "${var.s3_bucket_name}"
+  }
+}
